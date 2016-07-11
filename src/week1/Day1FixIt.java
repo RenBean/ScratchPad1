52f30d42ba5984aa38573391ce8ec06059051ae7
Ari Dalton <ren_logic@hotmail.com>
2016-07-11 14:46:31 -0600

    Initial commit

---
+++ src/week1/Day1FixIt.java
@@ -0,0 +1,30 @@
+package week1;
+
+import static java.lang.System.*;
+
+/**
+ * Created by Ari on 7/11/16.
+ */
+public class Day1FixIt {
+    private static int x = 12;
+    private static int y = 28;
+    private static double z = 3.3;
+
+    public static void main(String[] args) {
+        System.out.println("The answer to (x * y) / 3 is "+calculate());
+
+    }
+
+    private static double calculate() {
+        //double d = (x * y) / z;
+        //return d;
+        return (x * y) / z;
+
+
+    }
+
+
+}
+
+
+
---
+++ src/week1/day1/people/Person.java
@@ -0,0 +1,39 @@
+package week1.day1.people;
+
+/**
+ * Created by Ari on 7/11/16.
+ */
+public abstract class Person {
+
+    String name;
+    int age;
+    String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+
+
+
+}
