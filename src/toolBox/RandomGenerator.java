a6a16a97467a155a2ffa6f385fd2bf17ea768bd3
Ari Dalton <ren_logic@hotmail.com>
2016-07-14 17:06:10 -0600

    day 3 ?

---
+++ src/toolBox/RandomGenerator.java
@@ -0,0 +1,24 @@
+package toolBox;
+import java.util.Random;
+
+
+public class RandomGenerator {
+
+
+    public static int randomInt(int min, int max) {
+
+
+        Random random = new Random(System.currentTimeMillis());
+        // we just made Random more randowm with System.currentTimeMillis
+        int randomNumber = random.nextInt((max - min) + 1) + min;
+
+        return randomNumber;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(randomInt(5,12));
+
+
+    }
+}
--- src/week1/day3/Day3FibonacciWithRecursion.java
+++ src/week1/day3/Day3FibonacciWithRecursion.java
@@ -7,12 +7,13 @@ public class Day3FibonacciWithRecursion {
     static int n1=0, n2=1, n3=0;
 
     public static void main(String args[]){
+        Day3FibonacciWithRecursion day3FibonacciWithRecursion = new Day3FibonacciWithRecursion();
         int count=10;
-        System.out.print(n1+" "+n2);//printing 0 and 1
-        printFibonacci(count-2);//n-2 because 2 numbers are already printed
+        System.out.print(day3FibonacciWithRecursion.n1+" "+day3FibonacciWithRecursion.n2);//printing 0 and 1
+        day3FibonacciWithRecursion.printFibonacci(count-2);//n-2 because 2 numbers are already printed
     }
-
-    static void printFibonacci(int count){
+//static has been removed by the day3FibonacciWithRecursion instance.
+     void printFibonacci(int count){
         if(count>0){
             // Start by setting n3, n2, and n1
             n3 = n1 + n2;
