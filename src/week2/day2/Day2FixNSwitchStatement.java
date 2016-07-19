5b73e59d8141617c1eebc4be0971eac25f206f80
Ari Dalton <ren_logic@hotmail.com>
2016-07-19 11:47:16 -0600

    week2

---
+++ src/week2/day2/Day2FixNSwitchStatement.java
@@ -0,0 +1,52 @@
+package week2.day2;
+
+/**
+ * Created by Ari on 7/19/16.
+ */
+public class Day2FixNSwitchStatement {
+    private static char branch = 'C'; // C - CSE, E - ECE, M - Mech
+    private static int year = 2;
+
+    public static void main(String[] args) {
+        switchIt();
+    }
+
+    private static void switchIt() {
+        switch (year) {
+            case 1:
+                System.out.println("English, Math, Drawing");
+                break;
+            case 2:
+                switch (branch) // LINE C
+                {
+                    case 'C':
+                        System.out.println("Data structures, Java, Computer Organization");
+                        break;
+                    case 'E':
+                        System.out.println("Micro processors, Logic switching theory");
+                        break;
+                    case 'M':
+                        System.out.println("Drawing, Manufacturing Machines");
+                        break;
+                }
+                break;
+            case 3:
+                switch (branch) // LINE D
+                {
+                    case 'C':
+                        System.out.println("Operating System, RDBMS");
+                        break;
+                    case 'E':
+                        System.out.println("Fundamentals of Logic Design, Microelectronics");
+                        break;
+                    case 'Z':
+                        System.out.println("Internal Combustion Engines, Mechanical Vibration");
+                        break;
+                }
+                break;
+
+        }
+
+    }
+
+}
