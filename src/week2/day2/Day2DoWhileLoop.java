f753dc556877e05e2ab18e61395a8de19bc21196
Ari Dalton <ren_logic@hotmail.com>
2016-07-19 10:56:27 -0600

    week2

--- src/week2/day2/Day2DoWhileLoop.java
+++ src/week2/day2/Day2DoWhileLoop.java
@@ -6,14 +6,32 @@ package week2.day2;
 public class Day2DoWhileLoop {
     public static void main(String args[]) {
         // call the whileLoop method
+        whileLoop(5);
         System.out.println();
         //call the doWhileLoopMethod
+        doWhileLoop(5);
     }
 
     // create a new method called 'whileLoop' that takes an int called 'start'
-    // create a while loop that iterates from start to zero and prints out the result
+    public static void whileLoop (int start){
+        // create a while loop that iterates from start to zero and prints out the result
+        while (start >= 0 ) {
+            System.out.println(start);
+            start--;
+        }
+    }
+
 
     // create a new method called 'doWhileLoop' that takes an int called 'start'
-    // create a do-while loop that iterates from start to zero and prints out the result
+    public static void doWhileLoop (int start){
+        // create a do-while loop that iterates from start to zero and prints out the result
+        do {
+            System.out.println(start);
+            start--;
+        } while(start >= 0);{
+
+        }
+    }
+
 
 }
